b50dea72da5c9908c38e215a9fa0a7de2c9be91d
Sluchai40 <mabbolos8@gmail.com>
2024-01-02 10:40:19 +0100

    job04 done

--- jour01/job04.py
+++ jour01/job04.py
@@ -1,13 +1,15 @@
-# class Personne:
-#     nom = ""
-#     prenom = ""
-#     def SePresenter(self, nom, prenom):
-#         return(nom, prenom)
+class Personne:
+    def __init__(self):
+        self.nom = ""
+        self.prenom = ""
+    def SePresenter(self, nom, prenom):
+        return(nom, prenom)
     
-# presentation = Personne()
-# presentation.nom = 'Yrles'
-# presentation.prenom = 'Anthony'
-# presentation.SePresenter(presentation.nom, presentation.prenom)
-# print(f"Je m'appel {}")
+presentation = Personne()
+presentation.nom = input()
+presentation.prenom = input()
+presentation.SePresenter(presentation.nom, presentation.prenom)
+print(f"Mon nom est {presentation.nom} {presentation.prenom}")
+
 
 
