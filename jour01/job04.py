60ea2d9cdec247e933be1f452868eb6745a24fcb
Sluchai40 <mabbolos8@gmail.com>
2024-01-02 16:00:58 +0100

    some change in the job 04

--- jour01/job04.py
+++ jour01/job04.py
@@ -3,13 +3,13 @@ class Personne:
         self.nom = ""
         self.prenom = ""
     def SePresenter(self, nom, prenom):
-        return(nom, prenom)
+        return self.nom + ' ' + self.prenom
     
 presentation = Personne()
 presentation.nom = input()
 presentation.prenom = input()
 presentation.SePresenter(presentation.nom, presentation.prenom)
-print(f"Mon nom est {presentation.nom} {presentation.prenom}")
+print(f"Mon nom est {presentation.SePresenter(presentation.nom, presentation.prenom)}")
 
 
 
