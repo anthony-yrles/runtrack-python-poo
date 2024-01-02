4a640cdb9dc3626e44225c0fe54c1fb40541bd39
Sluchai40 <mabbolos8@gmail.com>
2024-01-02 10:06:58 +0100

    job 3 done

--- jour01/job03.py
+++ jour01/job03.py
@@ -1,8 +1,6 @@
 class Operation:
     def addition(self, nombre1, nombre2):
-        self.nombre1 = self.nombre1 + nombre1
-        self.nombre2 = self.nombre2 + nombre2
-        print(self.nombre1 + self.nombre2)
+        print(nombre1 + nombre2)
 
 result = Operation()
 result.addition(12, 3)
